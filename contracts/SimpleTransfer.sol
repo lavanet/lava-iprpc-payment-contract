de5b7d3e14735e930b6a74288a4f42453e836e31
Ran Mishael <ran@lavanet.xyz>
2023-10-19 18:33:23 +0200

    adding owner and backup ownership to the smart contract

--- contracts/SimpleTransfer.sol
+++ contracts/SimpleTransfer.sol
@@ -1,21 +1,56 @@
-
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// // Uncomment this line to use console.log
-import "hardhat/console.sol";
+import "hardhat/console.sol"; // Import console.sol for debugging
 
 contract SimpleTransfer {
-    function transfer(address payable _to, uint256 amount) external payable {
-        console.log("[TEST 1]", amount);
-        require(amount > 0, "Amount must be greater than 0");
-        console.log("[TEST 2]", address(msg.sender).balance);
-        console.log("[TEST 4]", address(this).balance);
-        
-        // Ensure the sender has enough balance to perform the transfer
-        require(address(msg.sender).balance >= amount, "Insufficient balance for the transfer");
+    address public owner;
+    address public backupOwner;
+    mapping(address => uint256) public balances;
 
-        // Transfer Ether from the sender to the recipient
-        _to.transfer(amount);
+    constructor() {
+        owner = msg.sender;
+        backupOwner = address(0);
+    }
+
+    function payProviders(Provider[] memory providers) public {
+        require(msg.sender == owner || msg.sender == backupOwner, "Only the owner / backup can call this function");
+        uint256 totalAmount = 0;
+        uint256 balanceAtStart = address(this).balance;
+        for (uint256 i = 0; i < providers.length; i++) {
+            address payable providerAddress = payable(providers[i].name);
+            uint256 amount = providers[i].value;
+            totalAmount += amount;
+            providerAddress.transfer(amount);
+            balances[providerAddress] += amount;
+            // Use console.log for debugging
+            console.log("Transferred", amount, "to", providerAddress);
+        }
+        require(balanceAtStart >= totalAmount, "Insufficient balance in the contract");
+        console.log("Balance left in contract:", address(this).balance);
+    }
+
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner || msg.sender == backupOwner, "Only the owner / backup can call this function");
+        owner = newOwner;
+    }
+
+    function setBackUpOwner(address newBackupOwner) public {
+        require(msg.sender == owner || msg.sender == backupOwner, "Only the owner / backup can call this function");
+        backupOwner = newBackupOwner;
+    }
+
+    receive() external payable {
+        // Allow anyone to add funds to the contract
+        require(msg.value > 0, "Value must be greater than 0");
+        // Log the contract's address and balance
+        console.log("Received", msg.value, "Wei from", msg.sender);
+        console.log("Contract Address:", address(this));
+        console.log("Contract Total Balance:", address(this).balance);
+    }
+
+    struct Provider {
+        address name;
+        uint256 value;
     }
 }
--- test/SimpleTransfer.ts
+++ test/SimpleTransfer.ts
@@ -1,42 +1,146 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import {
-    time,
-    loadFixture,
-  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { Contract, ContractFactory, parseEther } from "ethers";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { SimpleTransfer } from "../typechain-types/SimpleTransfer"
+
 
 describe("SimpleTransfer", function () {
     let owner: HardhatEthersSigner;
     let recipient: HardhatEthersSigner;
+    let recipient2: HardhatEthersSigner;
+    let simpleTransfer: SimpleTransfer;
+    // SimpleTransfer.ProviderStruct[]
 
-    async function setupBefore() {
-        [owner, recipient] = await ethers.getSigners();
-        const SimpleTransfer = await ethers.getContractFactory("SimpleTransfer");
-        const simpleTransfer = await SimpleTransfer.deploy();
-        return {simpleTransfer, owner, recipient};
-    };
+    before(async function () {
+        [owner, recipient, recipient2] = await ethers.getSigners();
+        const SimpleTransferFactory = await ethers.getContractFactory("SimpleTransfer");
+        simpleTransfer = await SimpleTransferFactory.deploy();
+    });
 
-    it("Should transfer funds", async function () {
-        const {simpleTransfer, owner, recipient} = await loadFixture(setupBefore);
-        
-        const initialBalance = await ethers.provider.getBalance(recipient.address);
+    it("Simple flow 2 accounts", async function () {
         const ownerBalanceBefore = await ethers.provider.getBalance(owner.address);
-        console.log("recipient initialBalance", initialBalance);
-        console.log("ownerBalanceBefore      ", ownerBalanceBefore);
-        const amount = parseEther("1");
-        console.log("Amount out              ", amount)
+        const initialRecipientBalance = await ethers.provider.getBalance(recipient.address);
+        const initialRecipient2Balance = await ethers.provider.getBalance(recipient2.address);
 
-        await owner.sendTransaction({
-            to: owner.address,
+        const amount = ethers.parseEther("1");
+
+        // Send some Ether to the owner to fund the contract
+        const fundTransaction = await owner.sendTransaction({
+            to: await simpleTransfer.getAddress(),
+            value: amount * (3n), // Make sure the contract has enough balance
         });
-        await simpleTransfer.connect(owner).transfer(recipient.address, amount);
 
-        const finalBalance = await ethers.provider.getBalance(recipient.address);
-        const ownerFinalBalance = await ethers.provider.getBalance(owner.address);
-        console.log("ownerFinalBalance", ownerFinalBalance)
-        console.log("recipient finalBalance", finalBalance)
-        // expect(finalBalance).to.equal(initialBalance.add(amount));
+        await fundTransaction.wait();
+
+        // Get the contract's balance before the transfer
+        const contractBalanceBefore = await ethers.provider.getBalance(await simpleTransfer.getAddress());
+        
+        // Transfer funds from the contract to the recipient
+        const transferTransaction = await simpleTransfer.connect(owner).payProviders(
+            [
+                {
+                    name: recipient.address,
+                    value: amount,
+                },
+                {
+                    name: recipient2.address,
+                    value: amount,
+                },
+            ]
+        );
+
+        await transferTransaction.wait();
+
+        const finalRecipientBalance = await ethers.provider.getBalance(recipient.address);
+        const finalRecipient2Balance = await ethers.provider.getBalance(recipient2.address);
+        const contractBalanceAfter = await ethers.provider.getBalance(await simpleTransfer.getAddress());
+        const ownerBalanceAfter = await ethers.provider.getBalance(owner.address);
+        console.log("initialRecipientBalance ", initialRecipientBalance)
+        console.log("initialRecipient2Balance", initialRecipient2Balance)
+        console.log("finalRecipientBalance   ", finalRecipientBalance)
+        console.log("finalRecipient2Balance  ", finalRecipient2Balance)
+        console.log("contractBalanceBefore   ", contractBalanceBefore)
+        console.log("contractBalanceAfter    ", contractBalanceAfter)
+        console.log("ownerBalanceBefore      ", ownerBalanceBefore)
+        console.log("ownerBalanceAfter       ", ownerBalanceAfter)
+
+        expect(contractBalanceBefore - (amount*2n)).to.equal(contractBalanceAfter); // Contract balance should decrease
+        expect(finalRecipientBalance - (amount)).to.equal(initialRecipientBalance); // Recipient should receive the transferred amount
+    });
+
+    it("Test many accounts", async function () {
+        const providerReceivers = (await ethers.getSigners()).slice(3,21); // get only accounts that didn't have money before.
+        console.log("Testing ", providerReceivers.length, "accounts")
+        const owner = (await ethers.getSigners())[0];
+        const payAmount = ethers.parseEther("1");
+        const fundTransaction = await owner.sendTransaction({
+            to: await simpleTransfer.getAddress(),
+            value: payAmount * (BigInt(providerReceivers.length)), // Make sure the contract has enough balance
+        });
+        await fundTransaction.wait();
+        const paymentListOfProviders: SimpleTransfer.ProviderStruct[] = [];
+        let totalBalanceBefore = 0n;
+        for (let address of providerReceivers) {
+            const balanceBefore = await ethers.provider.getBalance(address.address);
+            totalBalanceBefore += balanceBefore;
+            paymentListOfProviders.push({
+                name: await address.address, 
+                value: payAmount,
+            })
+        }
+         // Transfer funds from the contract to the recipient
+         const transferTransaction = await simpleTransfer.connect(owner).payProviders(
+            paymentListOfProviders
+        );
+        await transferTransaction.wait();
+        let totalBalanceAfter = 0n;
+        for (let address of providerReceivers) {
+            const balanceAfter = await ethers.provider.getBalance(address.address)
+            totalBalanceAfter += balanceAfter
+            console.log("balance after ", balanceAfter);
+        }
+        expect(totalBalanceAfter).to.equal(totalBalanceBefore + BigInt(providerReceivers.length)*payAmount)
+
+    });
+
+    it("Fund the contract with a different address", async function () {
+        const providerReceivers = (await ethers.getSigners()).slice(3,21); // get only accounts that didn't have money before.
+        console.log("Testing ", providerReceivers.length, "accounts")
+        const owner = (await ethers.getSigners())[0];
+        const payAmount = ethers.parseEther("1");
+        const recipientBalanceAtFirst = await ethers.provider.getBalance(recipient.address);
+        const ownerBalanceAtFirst = await ethers.provider.getBalance(owner.address);
+        const fundTransaction = await recipient.sendTransaction({ // let the recipient (in this test this is some random address) pay
+            to: await simpleTransfer.getAddress(),
+            value: payAmount * (BigInt(providerReceivers.length)), // Make sure the contract has enough balance
+        });
+        await fundTransaction.wait();
+        const paymentListOfProviders: SimpleTransfer.ProviderStruct[] = [];
+        // providersInput : SimpleTransfer.ProviderStruct[] = []
+        for (let address of providerReceivers) {
+            paymentListOfProviders.push({
+                name: await address.address, 
+                value: payAmount,
+            })
+        }
+         // Transfer funds from the contract to the recipient
+         const transferTransaction = await simpleTransfer.connect(owner).payProviders(
+            paymentListOfProviders
+        );
+        const result = await transferTransaction.wait();
+        for (let address of providerReceivers) {
+            console.log("balance after ", await ethers.provider.getBalance(address.address));
+        }
+
+        const gasUsed = result?.gasUsed;
+        if (!gasUsed) {
+            throw new Error("impossible to get here");
+        }
+        
+        const ownerBalanceAtFinal = await ethers.provider.getBalance(owner.address);
+        console.log(ownerBalanceAtFirst -ownerBalanceAtFinal)
+        // expect the owners funds to remain the same (minus gas) as the account named recipient payed
+        expect(ownerBalanceAtFinal+transferTransaction.gasPrice*gasUsed).to.equal(ownerBalanceAtFirst) 
+        expect(recipientBalanceAtFirst).to.above(await ethers.provider.getBalance(recipient.address))
     });
 });
